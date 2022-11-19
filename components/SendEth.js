ce6964c0cda122e45c3d18a1823dbaebdeb1f024
Byldr <ryan@fueledonbacon.com>
2022-11-19 14:30:34 -0500

    change eth and usdc inputs

--- components/SendEth.js
+++ components/SendEth.js
@@ -1,17 +1,37 @@
-import { useAccount, useSendTransaction, usePrepareSendTransaction } from 'wagmi'
+import { useState } from 'react';   
+import { useSendTransaction, usePrepareSendTransaction } from 'wagmi'
 import { ethers } from 'ethers'
 
-export default function SendEth(props) {
-    const { amount } = props
+export default function SendEth() {
+    const [amount, setAmount] = useState('0.01')
     const { config } = usePrepareSendTransaction({
         request: {
             to: 'byldr.eth',
-            value: ethers.utils.parseEther( amount || '0.01')
+            value: ethers.utils.parseEther(amount || '0.01')
         }
     })
     const { data, isLoading, isSuccess, sendTransaction } = useSendTransaction(config)
+    const validPattern="^[0-9]*\.?[0-9]{0,4}$" 
+    const isValidInput = (amount) => {
+        return RegExp(validPattern).test(amount)
+    }
+
     return (<>
-        <button className="bg-blue-500 p-4 rounded-lg" disabled={!sendTransaction} onClick={() => sendTransaction?.()}>Send {amount} ETH</button>
+        <input className="p-4 rounded-lg mr-4 " type="text" pattern={validPattern} onChange={(e) => {
+            if (isValidInput(e.target.value)){
+                if(e.target.value == ".")
+                    setAmount("0.")
+                else {
+                    setAmount(e.target.value)
+                }
+            }
+
+        }} value={amount} />
+        <button className="bg-blue-500 p-4 rounded-lg disabled:bg-gray-400 min-w-[11em]"
+            disabled={!sendTransaction}
+            onClick={() => sendTransaction?.()}>
+                Send {amount} ETH
+        </button>
         {isLoading && <div>Check Wallet</div>}
         {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
     </>)
--- components/SendUSDC.js
+++ components/SendUSDC.js
@@ -1,21 +1,34 @@
+import { useState } from 'react';
 import { ethers } from 'ethers'
-import { useAccount, erc20ABI, useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { erc20ABI, useContractWrite, usePrepareContractWrite } from 'wagmi'
 
-export default function SendUSDC(props) {
-    const { amount } = props
-    const { address } = useAccount()
+export default function SendUSDC() {
+    const [amount, setAmount] = useState('10')
 
     const { config } = usePrepareContractWrite({
         address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
         abi: erc20ABI,
         functionName: 'transfer',
-        args: ['byldr.eth', ethers.utils.parseUnits(amount || '1', 6)],
+        args: ['byldr.eth', ethers.utils.parseUnits(amount || '0', 6)],
     })
     const { data, isLoading, isSuccess, write } = useContractWrite(config)
+    const validPattern = "^[0-9]*\.?[0-9]{0,4}$"
+    const isValidInput = (amount) => {
+        return RegExp(validPattern).test(amount)
+    }
 
     return (
         <div>
-            <button className="bg-blue-500 rounded-lg p-4 mt-2" disabled={!write} onClick={() => write?.()}>
+            <input className="p-4 rounded-lg mr-4 " type="text" pattern={validPattern} onChange={(e) => {
+                if (isValidInput(e.target.value)){
+                    if(e.target.value == ".")
+                        setAmount("0.")
+                    else {
+                        setAmount(e.target.value)
+                    }
+                }
+            }} value={amount} />
+            <button className="bg-blue-500 rounded-lg p-4 mt-2 cursor-pointer disabled:bg-gray-400 min-w-[11em]" disabled={!write} onClick={() => write?.()}>
                 Send {amount} USDC
             </button>
             {isLoading && <div>Check Wallet</div>}
